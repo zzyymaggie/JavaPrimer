483fecd3590819f6a11c9523231247415fb593cf
Sophia <sophia@SophiadeMacBook-Air.local>
2017-08-23 23:12:05 +0800

    update file format

--- src/main/java/xyz/zzyymaggie/spring/aop/App.java
+++ src/main/java/xyz/zzyymaggie/spring/aop/App.java
@@ -7,22 +7,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * spring aop demo code
  *
  */
-public class App 
-{
+public class App {
 	private static ApplicationContext context;
-	
-    public static void main( String[] args )
-    {
-    		context = getApplicationContext();
-    			TestServiceInter ts=(TestServiceInter) context.getBean("proxyFactoryBean");
-    			ts.sayHello();
-    			((TestServiceInter2)ts).sayBye();
-    }
-    
-    public static ApplicationContext getApplicationContext() {
-    		if(context == null) {
-    			context = new ClassPathXmlApplicationContext("beans-aop.xml");
-    		}
-    		return context;
-    }
+
+	public static void main(String[] args) {
+		context = getApplicationContext();
+		TestServiceInter ts = (TestServiceInter) context.getBean("proxyFactoryBean");
+		ts.sayHello();
+		((TestServiceInter2) ts).sayBye();
+	}
+
+	public static ApplicationContext getApplicationContext() {
+		if (context == null) {
+			context = new ClassPathXmlApplicationContext("beans-aop.xml");
+		}
+		return context;
+	}
 }
