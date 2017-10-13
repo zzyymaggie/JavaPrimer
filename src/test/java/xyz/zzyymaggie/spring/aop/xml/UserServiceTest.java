a56af3778ba71451849f02e31170e09fe56064eb
Sophia <sophia@SophiadeMacBook-Air.local>
2017-10-13 23:02:29 +0800

    add spring aop by xml and annotation demo

---
+++ src/test/java/xyz/zzyymaggie/spring/aop/xml/UserServiceTest.java
@@ -0,0 +1,30 @@
+package xyz.zzyymaggie.spring.aop.xml;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import xyz.zzyymaggie.spring.model.User;
+import xyz.zzyymaggie.spring.service.UserService;
+
+public class UserServiceTest {
+    private static ClassPathXmlApplicationContext context;
+    
+	@Test
+	public void testAdd() throws Exception {
+	    context = getApplicationContext();
+		UserService service = (UserService)context.getBean("userService");
+		
+		User u = new User();
+		u.setUsername("zhangsan");
+		u.setPassword("zhangsan");
+		service.add(u);
+		context.destroy();
+	}
+	
+	public static ClassPathXmlApplicationContext getApplicationContext() {
+        if (context == null) {
+            context = new ClassPathXmlApplicationContext("beans-aop-xml.xml");
+        }
+        return context;
+    }
+}
