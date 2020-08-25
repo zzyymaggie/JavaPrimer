042a48cf4c9f6df0f00ce4bdb2e5daf57a3a8802
zhangyu20 <zhangyu20@sunlands.com>
2020-08-25 16:16:46 +0800

    添加aqs学习代码

---
+++ src/main/java/xyz/zzyymaggie/aqs/Example1.java
@@ -0,0 +1,26 @@
+package xyz.zzyymaggie.aqs;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * 采用park实现线程同步代码演示
+ * @see https://www.bilibili.com/video/BV1d4411m7Pa
+ */
+public class Example1 {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("-------------m1----------------");
+        Thread t1 = new Thread("t1") {
+            @Override
+            public void run() {
+                System.out.println("----------------t1---------------");
+                LockSupport.park();
+                System.out.println("---------------t2-----------------");
+            }
+        };
+        t1.start();
+
+        Thread.sleep(5000);
+        System.out.println("--------------------m2--------------");
+        LockSupport.unpark(t1);
+    }
+}
